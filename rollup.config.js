0b8c4538d116c66edf84def5c540eb717dec990f
Michael Airola <mairola@defie.co>
2020-02-21 12:54:00 -0800

    rollup production and development bundles are now slightly different

--- rollup.config.js
+++ rollup.config.js
@@ -3,17 +3,21 @@ import babel from 'rollup-plugin-babel';
 import minifyHTML from 'rollup-plugin-minify-html-literals';
 import { uglify } from 'rollup-plugin-uglify';
 
-const config = {
-	input: 'src/widget.js',
-	output: [
-		{ file: 'dist/widget.js', format: 'iife' },
-	],
-	plugins: [
-		minifyHTML(),
-		resolve(),
-		babel({ presets: [ "@babel/preset-env" ] }),
-		uglify()
+const DevMode = process.env.DEV_MODE == "true"
+
+const config = () => {
+	const input = 'src/widget.js';
+	const output = [ { file: 'dist/widget.js', format: 'iife' } ];
+	let plugins = [ 
+		resolve(), 
+		babel({ presets: [ "@babel/preset-env" ] }) 
 	]
+	plugins = DevMode ? plugins : [ 
+		minifyHTML(),
+		...plugins,
+		uglify(),
+	]
+	return { input, output, plugins }
 };
 
 export default config;
